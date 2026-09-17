064fe359e36c3f58a9282ad0270cb31ded2aef26
badardfday <barfing.lmao1@gmail.com>
2026-09-17 21:16:47 +0800

    Update 155615604.lua

--- games/155615604.lua
+++ games/155615604.lua
@@ -1355,6 +1355,40 @@ run(function()
 end)
 
 run(function()
+	local NoSit
+	local humanoid
+
+	local function EntityAdded(entity)
+		humanoid = entity.Humanoid
+		humanoid:SetStateEnabled(Enum.HumanoidStateType.Seated, false)
+		humanoid.Sit = false
+		humanoid:ChangeState(Enum.HumanoidStateType.GettingUp)
+
+		NoSit:Clean(humanoid.StateChanged:Connect(function(_, state)
+			if state == Enum.HumanoidStateType.Seated then
+				humanoid.Sit = false
+				humanoid:ChangeState(Enum.HumanoidStateType.GettingUp)
+			end
+		end))
+	end
+
+	NoSit = vape.Categories.Utility:CreateModule({
+		Name = 'NoSit',
+		Function = function(callback)
+			if callback then
+				NoSit:Clean(entitylib.Events.LocalAdded:Connect(EntityAdded))
+				if entitylib.isAlive then
+					EntityAdded(entitylib.character)
+				end
+			elseif humanoid then
+				humanoid:SetStateEnabled(Enum.HumanoidStateType.Seated, true)
+			end
+		end,
+		Tooltip = 'Prevents your character from sitting.'
+	})
+end)
+
+run(function()
 	local AutoArrest
 	local Range
 	local HandCheck
