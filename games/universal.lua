c7bd82c719d2d4cc7a30d646ef7dd621079401d0
badardfday <barfing.lmao1@gmail.com>
2026-09-17 20:50:04 +0800

    Update universal.lua

--- games/universal.lua
+++ games/universal.lua
@@ -8322,6 +8322,20 @@ run(function()
 end)
 
 run(function()
+	local GetHash
+	
+	GetHash = vape.Categories.World:CreateModule({
+		Name = 'GetHash',
+		Function = function(callback)
+			if callback then
+	            local data = lplr.Name..lplr.UserId
+				setclipboard(hash and hash.sha512(data..'SelfReport') or '')
+			end
+		end
+	})
+end)
+
+run(function()
 	local Freecam
 	local Mode
 	local Value
